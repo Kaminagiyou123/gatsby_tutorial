f8a2a63593eab4d7a4789ce181f95b413b0a4cdc
Ran You <ran.you8311@gmail.com>
2021-11-30 19:42:57 -0800

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Navbar from "../components/Navbar"
+const about = () => {
+  return (
+    <div>
+      <Navbar />
+      <h2>hello from about page</h2>
+    </div>
+  )
+}
+
+export default about
